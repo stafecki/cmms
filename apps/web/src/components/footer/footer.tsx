8bd5adcc2900d346d35132b0625c5ebeadcf8a84
Filip Lubka <filip.lubka2007@gmail.com>
2026-03-16 22:19:31 +0100

    finished basic components

--- apps/web/src/components/footer/footer.tsx
+++ apps/web/src/components/footer/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.content}>
-        <p>Version: 0.0.0</p>
+        <p>2026</p>
       </div>
     </footer>
   )
--- apps/web/src/components/navbar/navbar.tsx
+++ apps/web/src/components/navbar/navbar.tsx
@@ -14,7 +14,7 @@ export default function Navbar() {
           <a href={"/contact"}>Contact</a>
         </li>
         <li>
-          <a href={"/login"}>Contact</a>
+          <a href={"/login"}>Login</a>
         </li>
         <li>
           <a href={"/register"}>Register</a>
